4bdcd9dbc90f6245338e2cf9a1adc5ddcf9e673c
Bhavesh Patel <patel.baps@gmail.com>
2016-03-19 17:58:35 +0530

    added new projects
    added new projects

---
+++ YogisabhaGruhServices/IBookingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace YSGServices
+{
+    IEnumerable<Comment> Get();
+    bool TryGet(int id, out Comment comment);
+    Comment Add(Comment comment);
+    bool Delete(int id);
+    bool Update(Comment comment);
+}
