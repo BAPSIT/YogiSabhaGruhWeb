e80ea4f309a20620b3a59de5b4921422acac929d
Jigar Gandhi <jigarr.gandhi@gmail.com>
2016-03-25 14:09:54 +0530

    adding tithi files
    Adding tithi files

--- YSGOpsWeb/Class/Common.cs
+++ YSGOpsWeb/Class/Common.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using System.Web;
-using YSGOps.DataModel;
-using YSGServices;
+//using YSGOps.DataModel;
+//using YSGServices;
 
 namespace YSGOpsWeb.Class
 {
@@ -12,22 +12,22 @@ namespace YSGOpsWeb.Class
     {
         public static List<YSG_Booking_Info> GetAllBooking()
         {
-            List<YSG_Booking_Info> result = null;
+        //    List<YSG_Booking_Info> result = null;
 
-            var proxy = ServiceFactory.GetService<IBookingService>();
-            try
-            {
-                    result = proxy.GetAllBooking();
-            }
-            catch (Exception exception)
-            {
-                //Handle exception
-            }
-            finally
-            {
-                ServiceFactory.CloseOrAbort((ICommunicationObject)proxy);
-            }
-            return result;
+        //    var proxy = ServiceFactory.GetService<IBookingService>();
+        //    try
+        //    {
+        //            result = proxy.GetAllBooking();
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        //Handle exception
+        //    }
+        //    finally
+        //    {
+        //        ServiceFactory.CloseOrAbort((ICommunicationObject)proxy);
+        //    }
+        //    return result;
         }
     }
 }
---
+++ YSGOpsWeb/Pages/TithiCalendar.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TithiCalendar.aspx.cs"
+    Inherits="YSGOpsWeb.Pages.TithiCalendar" MasterPageFile="~/Master/YSGWeb.Master" %>
+
+<asp:Content runat="server" ContentPlaceHolderID="head">
+</asp:Content>
+<asp:Content runat="server" ContentPlaceHolderID="ContentPlaceHolder1">
+    <div class="panel panel-default">
+        <div class="panel-heading">Upload Tithi CSV File</div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <div class="form-group">
+                        <label>Choose File:</label>
+                        <asp:FileUpload ID="flUpTithi" runat="server" />
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <div class="form-group">
+                        <p>Click on the button below to download the current tithi information</p>
+                        <asp:Button Text="Download Current Data" CssClass="btn btn-default" runat="server" />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
+
